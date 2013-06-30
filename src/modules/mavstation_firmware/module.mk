5e54712a382a0562af0030006ce73f7a483ec039
Pat Hickey <pat@galois.com>
2013-06-30 14:19:55 -0700

    mavstation firmware: clean out sbus, dsm, ppm dependencies

--- src/modules/mavstation_firmware/module.mk
+++ src/modules/mavstation_firmware/module.mk
@@ -2,12 +2,10 @@
 
 SRCS		= adc.c \
 		  controls.c \
-		  dsm.c \
 		  i2c.c \
 		  mixer.c \
 		  mavstation.c \
 		  registers.c \
 		  safety.c \
-		  sbus.c \
 		  ../systemlib/up_cxxinitialize.c \
 		  ../systemlib/perf_counter.c
